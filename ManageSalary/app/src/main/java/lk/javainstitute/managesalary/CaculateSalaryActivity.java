1f3797117c13081166b370008f5ea79eaa879506
Supun Sulakshana <supunsulakshana2001@gmail.com>
2025-01-15 20:55:55 +0530

    update add product design
    add the product part adding process in addproduct activity

--- ManageSalary/app/src/main/java/lk/javainstitute/managesalary/CaculateSalaryActivity.java
+++ ManageSalary/app/src/main/java/lk/javainstitute/managesalary/CaculateSalaryActivity.java
@@ -1,8 +1,6 @@
 package lk.javainstitute.managesalary;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,39 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class AddProductActivity extends AppCompatActivity {
+public class CaculateSalaryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_add_product);
+        setContentView(R.layout.activity_caculate_salary);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Spinner spinner1 = findViewById(R.id.spinner);
-
-        List<String> products = new ArrayList<>();
-        products.add("Select Products");
-        products.add("Plain Pant");
-        products.add("Waist Band Pant");
-        products.add("Morden Pant");
-
-
-        ArrayAdapter<String> productArrayAdapter = new ArrayAdapter<>(
-                AddProductActivity.this
-                , R.layout.products_layout
-                , products
-        );
-
-        spinner1.setAdapter(productArrayAdapter);
-
-
     }
 }
